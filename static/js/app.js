5a7ad67ba9e9eda834d33f5afee1779ea40da12b
Claira J <julia.claira@gmail.com>
2021-07-02 11:30:41 -0700

    finished layout of bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -14,18 +14,34 @@ d3.json("samples.json").then((importedData)=>{
         for (i=0; i<10; i++){
             var tempObject={};
             tempObject['sample']=data.samples[0].sample_values[i];
-            tempObject['id']=data.samples[0].otu_ids[i];
+            tempObject['id']=`OTU ${data.samples[0].otu_ids[i]}`;
             tempObject['label']=data.samples[0].otu_labels[i];
             //tempObject.push({id:data.samples[0].otu_ids[i]});
             //tempObject.push({label:data.samples[0].otu_labels[i]});
-         
+            
             tempSampleValues.push(tempObject);
         }
 
         return tempSampleValues;
     };
     //Main ------------------------------------------------
-    console.log(topOTU());
+   
+    //Bar Graph
+    var dataTopTen=topOTU();
+    
+    var trace1 = {
+        type: 'bar',
+        x: dataTopTen.map(row=>row.sample).reverse(),
+        y: dataTopTen.map(row=>row.id.toString()).reverse(),
+        text: dataTopTen.map(row=>row.label).reverse(),
+        orientation: 'h',
+    }
+    var myData=[trace1];
 
+    layout = {
+        title: "Top Ten OTUs",
+    }
+    Plotly.newPlot("bar", myData,layout);
 
+    console.log(trace1)
 });
