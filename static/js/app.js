0c63f0699aa8c9740bbc6e567314497dd05488eb
Claira J <julia.claira@gmail.com>
2021-07-03 19:22:58 -0700

    dropbox updates bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -8,26 +8,90 @@ d3.json("samples.json").then((importedData)=>{
 
     //Functions------------------------------------------------
 
-    //Top 10 OTUs found in individual
+    
     function topOTU(){
         var tempSampleValues=[];
-        var testSubject=idField.property("value");
+        var testSubject=idField.property("value");//the index
         
-        for (i=0; i<10; i++){
+        //BAR GRAPH TOP TEN-----------------------------
+        //checks for samples with less than 10 rows of data
+        if(data.samples[testSubject].otu_ids.length<10){
+            var count=data.samples[testSubject].otu_ids.length;
+        }
+        else {
+            var count=10;
+        }
+        
+        //pulls top ten samples (or less if there isn't 10 samples)
+        for (i=0; i<count; i++){
             var tempObject={};
             tempObject['sample']=data.samples[testSubject].sample_values[i];
             tempObject['id']=`OTU ${data.samples[testSubject].otu_ids[i]}`;
             tempObject['label']=data.samples[testSubject].otu_labels[i];
 
-            tempSampleValues.push(tempObject);
+            tempSampleValues.push(tempObject);     
         }
-        console.log(tempSampleValues);
-        return tempSampleValues;
+
+        //BUBBLE GRAPH----------------
+        count=data.samples[testSubject].otu_ids.length;
+        for (i=0;i<count;i++){
+            var tempObject={};
+            tempObject['sample']=data.samples[testSubject].sample_values[i];
+            tempObject['id']=`OTU ${data.samples[testSubject].otu_ids[i]}`;
+            tempObject['label']=data.samples[testSubject].otu_labels[i];
+
+
+
+        }
+        //DRAW GRAPHS
+        createBar(tempSampleValues);//bar graph
     };
 
-    //function createBar(dataTopTen){}
+    function createBar(dataTopTen){
+    
+        //Adds line breaks instead of semicolons for hover text
+        var theLabel=dataTopTen.map(row=> {
+            return row.label.split(';');
+        })
+        theLabel=theLabel.map(row =>row.join('<br>'));
+        theLabel.reverse();
 
- 
+        //Creates Trace
+        var trace1 = {
+            type: 'bar',
+            mode: 'lines+markers',
+            x: dataTopTen.map(row=>row.sample).reverse(),
+            y: dataTopTen.map(row=>row.id.toString()).reverse(),
+            text: theLabel,//dataTopTen.map(row=>row.label),
+            hovertemplate: 
+            '<b>%{x}<b>'+
+            '<br>-------<br>'+
+            '<i>%{text}</i>'+
+            '<extra></extra>',
+            orientation: 'h',
+        };
+        var myData=[trace1];
+
+        //Layout and Plot
+        layout = {
+            title: {text:"Top Ten Bacteria Cultures Found",xanchor:'right'},
+            title_x: 0,
+            autosize: false,
+            width: 600,
+            height: 500,
+            margin: {
+                l: 70,
+                r: 200,
+                b: 100,
+                t: 30,
+                pad: 4
+            },
+            xanchor: 'left',
+            display:'none'
+        }
+        Plotly.newPlot("bar", myData,layout,{displayModeBar: false});
+    };
+
     //Main ------------------------------------------------
     
     //The Dropbox Selection
@@ -35,54 +99,10 @@ d3.json("samples.json").then((importedData)=>{
     Object.entries(data.samples).forEach(([key,value]) => {
         idField.append("option").text(value.id).attr('value',key);
     });
-    
     idField.on("change", topOTU);
-    console.log(idField.property("value"));
-//-------move the below into a function
-     //Bar Graph
-    var dataTopTen=topOTU();
-    
-    //Adds line breaks instead of semicolons for hover text
-    var theLabel=dataTopTen.map(row=> {
-        return row.label.split(';');
-    })
-    theLabel=theLabel.map(row =>row.join('<br>'));
-    theLabel.reverse();
 
-    //Creates Trace
-    var trace1 = {
-        type: 'bar',
-        mode: 'lines+markers',
-        x: dataTopTen.map(row=>row.sample).reverse(),
-        y: dataTopTen.map(row=>row.id.toString()).reverse(),
-        text: theLabel,//dataTopTen.map(row=>row.label),
-        hovertemplate: 
-        '<b>%{x}<b>'+
-        '<br>-------<br>'+
-        '<i>%{text}</i>'+
-        '<extra></extra>',
-        orientation: 'h',
-    };
-    var myData=[trace1];
-
-    //Layout and Plot
-    layout = {
-        title: {text:"Top Ten Bacteria Cultures Found",xanchor:'right'},
-        title_x: 0,
-        autosize: false,
-        width: 600,
-        height: 500,
-         margin: {
-            l: 70,
-            r: 200,
-            b: 100,
-            t: 30,
-            pad: 4
-        },
-        xanchor: 'left',
-        display:'none'
-    }
-    Plotly.newPlot("bar", myData,layout,{displayModeBar: false});
+    //pull data and initialize bar graph
+    topOTU();
 
     
 });
